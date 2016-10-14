56ee64487f9e206036f56aa155e3fdf7d3630fe7
Andy Best <andybest.net@gmail.com>
2016-10-14 17:21:00 +0100

    Updated to support ACIA

--- target_software/rom/build.sh
+++ target_software/rom/build.sh
@@ -2,4 +2,4 @@
 
 dasm  main.dasm -lbuild/romimage-list.txt -obuild/romimage.bin
 ftohex 1 build/romimage.bin build/romimage.hex
-../utils/bin2coe.py build/romimage.bin build/romimage.coe --skip_bytes=2
+../utils/bin2coe.py build/romimage.bin build/romimage.vbin --skip_bytes=2
